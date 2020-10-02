933ac45bb9d513a07e2cad53fc539baed2922696
ishu7599 <66133438+ishu7599@users.noreply.github.com>
2020-10-02 10:07:48 +0530

    Update data.js

--- es6 slider/data.js
+++ es6 slider/data.js
@@ -5,19 +5,19 @@ const people = [
     name: "peter doe",
     job: "product manager",
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
-  },
+  };
   {
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-2_ipcjws.jpg",
     name: "susan doe",
-    job: "developer",
+    job: "designer",
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
   },
   {
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-3_rxtqvi.jpg",
     name: "emma doe",
-    job: "designer",
+    job: "deceloper",
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
   },
 ];
